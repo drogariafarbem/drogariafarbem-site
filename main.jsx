c42bc007033e9852f61649296545d9c760d4f384
drogariafarbem <farbemdrogaria@gmail.com>
2025-09-27 18:02:23 -0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import logo from "./assets/logo.png"; // certifique-se que sua logo está em src/assets/logo.png
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const produtos = ["Dipirona", "Paracetamol", "Ibuprofeno"];
+
+  return (
+    <div className="app">
+      {/* Cabeçalho */}
+      <header className="header">
+        <img src={logo} alt="Logo Drogaria Farbém" className="logo" />
+        <h1 className="titulo">Drogaria Farbém</h1>
+      </header>
+
+      {/* Lista de produtos */}
+      <main className="conteudo">
+        <h2 className="subtitulo">💊 Lista de Produtos</h2>
+        <ul className="lista">
+          {produtos.map((produto, index) => (
+            <li key={index} className="item">
+              {produto}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
 
