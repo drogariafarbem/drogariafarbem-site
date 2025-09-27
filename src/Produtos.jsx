f1ac98d57022bcd9d9a03fa7f926bdc6c6215996
drogariafarbem <farbemdrogaria@gmail.com>
2025-09-27 15:27:53 -0300

    Create Produtos.jsx

---
+++ src/Produtos.jsx
@@ -0,0 +1,12 @@
+export default function Produtos() {
+  return (
+    <div>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        <li>Dipirona</li>
+        <li>Paracetamol</li>
+        <li>Ibuprofeno</li>
+      </ul>
+    </div>
+  );
+}
